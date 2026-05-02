24e48513f4c6535deae2a7c7fa65fa08d35fe81f
Kheyransa <xeyransaqorcuyeva@gmail.com>
2026-05-02 17:45:58 +0000

    Task 2: Matrix shape function

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Calculates the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of integers"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
