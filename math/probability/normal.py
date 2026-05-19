3a8aba4dc2804fe0ba22d0b4ccb8700b458e3d18
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-19 23:20:57 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -22,3 +22,11 @@ class Normal:
 
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return float((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return float(z * self.stddev + self.mean)
