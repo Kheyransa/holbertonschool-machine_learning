f8eb135b0085b50d22d37b7f74bc006622f16c9e
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-22 20:14:21 +0400

    Update multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -19,3 +19,24 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         data_centered = data - self.mean
         self.cov = np.dot(data_centered, data_centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """Calculates the PDF at a given data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+
+        x_centered = x - self.mean
+
+        norm_factor = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        exponent = -0.5 * np.dot(np.dot(x_centered.T, inv), x_centered)
+        pdf_val = norm_factor * np.exp(exponent)
+
+        return float(pdf_val[0][0])
