6ce7b674420e9533ed6eb9c400274944a8d94fae
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-26 18:55:30 +0400

    Update 0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import pandas as pd
-import string
 
 
 def from_numpy(array):
     num_cols = array.shape[1]
-    column_names = list(string.ascii_uppercase[:num_cols])
+    column_names = [chr(65 + i) for i in range(num_cols)]
     return pd.DataFrame(array, columns=column_names)
