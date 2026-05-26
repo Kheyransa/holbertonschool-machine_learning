8d9f9c1199955bb7c8f48ca6e8edcc4bcaeb9c94
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-26 20:48:14 +0400

    Update 11-concat.py

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,7 +3,7 @@
 Defines a function that concatenates two pd.DataFrames
 """
 import pandas as pd
-index_method = __import__('10-index').index
+index = __import__('10-index').index
 
 
 def concat(df1, df2):
@@ -18,12 +18,9 @@ def concat(df1, df2):
     Returns:
         pd.DataFrame: The concatenated DataFrame
     """
-    # 1. Hər iki dataframe-i Timestamp sütununa görə indeksləyirik
-    df1_indexed = index_method(df1)
-    df2_indexed = index_method(df2)
+    df1_indexed = index(df1)
+    df2_indexed = index(df2)
 
-    # 2. df2-dən 1417411920 də daxil olmaqla bütün üst sətirləri seçirik
     df2_filtered = df2_indexed.loc[:1417411920]
 
-    # 3. df2_filtered-i df1-in üzərinə əlavə edib keys təyin edirik
     return pd.concat([df2_filtered, df1_indexed], keys=['bitstamp', 'coinbase'])
