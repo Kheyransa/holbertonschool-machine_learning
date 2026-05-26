d92798f719a5fbc19d2342fc22290a82ac395f08
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-26 18:58:03 +0400

    Update 0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -6,3 +6,8 @@ def from_numpy(array):
     num_cols = array.shape[1]
     column_names = [chr(65 + i) for i in range(num_cols)]
     return pd.DataFrame(array, columns=column_names)
+
+
+from_numpy.__doc__ = "Creates a pd.DataFrame from a np.ndarray"
+import sys
+sys.modules[__name__].__doc__ = "Defines from_numpy function"
