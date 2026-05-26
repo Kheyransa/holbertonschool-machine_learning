1fba56e89bb64b033cd315f54e04b37682a71be0
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-26 19:00:00 +0400

    Update 0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+"""
+Defines a function that creates a pd.DataFrame from a np.ndarray
+"""
 import pandas as pd
 
 
 def from_numpy(array):
+    """
+    Creates a pd.DataFrame from a np.ndarray with capitalized column labels
+    """
     num_cols = array.shape[1]
     column_names = [chr(65 + i) for i in range(num_cols)]
     return pd.DataFrame(array, columns=column_names)
-
-
-from_numpy.__doc__ = "Creates a pd.DataFrame from a np.ndarray"
-import sys
-sys.modules[__name__].__doc__ = "Defines from_numpy function"
