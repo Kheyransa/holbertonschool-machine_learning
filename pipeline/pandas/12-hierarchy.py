3dda6c68998713d82ef34bf1d20f3f59bc10614f
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-26 20:15:46 +0400

    Update 12-hierarchy.py

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -3,7 +3,7 @@
 Defines a function that creates a MultiIndex hierarchy with Timestamp on top
 """
 import pandas as pd
-index_method = __import__('10-index').index
+index = __import__('10-index').index
 
 
 def hierarchy(df1, df2):
@@ -18,18 +18,11 @@ def hierarchy(df1, df2):
     Returns:
         pd.DataFrame: The rearranged and sliced MultiIndex DataFrame
     """
-    # 1. Hər iki dataframe-i Timestamp-ə görə indeksləyirik
-    df1_indexed = index_method(df1)
-    df2_indexed = index_method(df2)
+    df1_indexed = index(df1)
+    df2_indexed = index(df2)
 
-    # 2. Birləşdiririk (Öncə bitstamp, sonra coinbase gəlməlidir)
     df = pd.concat([df2_indexed, df1_indexed], keys=['bitstamp', 'coinbase'])
-
-    # 3. İndeks səviyyələrinin yerini dəyişirik (Timestamp önə keçir)
     df = df.swaplevel(0, 1, axis=0)
-
-    # 4. İndeksləri xronoloji olaraq artan sıra ilə çeşidləyirik
     df = df.sort_index()
 
-    # 5. Müəyyən olunmuş zaman aralığını filtrləyirik
     return df.loc[1417411980:1417417980]
