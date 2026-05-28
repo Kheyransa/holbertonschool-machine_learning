0d9f60e8e7a597409075567e3df544439d1f7c8b
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-28 20:30:04 +0400

    Create 1-scatter.py

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Defines a function that plots a scatter plot
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Plots height vs weight as a magenta scatter plot with custom labels
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.scatter(x, y, color='m')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+    plt.show()
