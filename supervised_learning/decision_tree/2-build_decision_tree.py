4fa30ed3cce310375aa1af2ea43af9950b96fc07
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-06-25 16:09:35 +0400

    Update 2-build_decision_tree.py

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -75,7 +75,7 @@ class Node:
         new_text = "+--" + lines[0] + "\n"
         for x in lines[1:]:
             if x:
-                new_text += ("  " + x) + "\n"
+                new_text += x + "\n"
         return new_text
 
     def __str__(self):
