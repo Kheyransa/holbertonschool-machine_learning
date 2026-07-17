2d8b476524e2e6062aa38afab63b352b14ca78f6
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-07-17 16:35:15 +0300

    Create 1-input.py

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Builds a neural network using the Keras Functional API.
+"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network.
+
+    Args:
+        nx: number of input features
+        layers: list containing the number of nodes in each layer
+        activations: list containing the activation functions for each layer
+        lambtha: L2 regularization parameter
+        keep_prob: probability that a node will be kept
+
+    Returns:
+        A Keras model.
+    """
+
+    inputs = K.Input(shape=(nx,))
+    x = inputs
+
+    for i in range(len(layers)):
+        x = K.layers.Dense(
+            units=layers[i],
+            activation=activations[i],
+            kernel_initializer=K.initializers.HeNormal(),
+            kernel_regularizer=K.regularizers.L2(lambtha)
+        )(x)
+
+        if i != len(layers) - 1:
+            x = K.layers.Dropout(rate=1 - keep_prob)(x)
+
+    model = K.Model(inputs=inputs, outputs=x)
+
+    return model
