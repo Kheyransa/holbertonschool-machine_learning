6b7dd01b6870dcdd90a2cf5722bc61ce0e72fd52
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-08-15 11:01:02 +0400

    Update 4-train.py

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,25 +3,32 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                verbose=True, shuffle=False):
+def train_model(
+        network, data, labels, batch_size, epochs,
+        verbose=True, shuffle=False):
     """
     Trains a model using mini-batch gradient descent
 
-    network -- the model to train
-    data -- numpy.ndarray of shape (m, nx) containing the input data
-    labels -- one-hot numpy.ndarray of shape (m, classes) with the labels
-    batch_size -- size of the batch used for mini-batch gradient descent
-    epochs -- number of passes through data for mini-batch gradient descent
-    verbose -- boolean that determines if output should be printed
-    shuffle -- boolean that determines whether to shuffle the batches
-    every epoch
+    network: the model to train
+    data: numpy.ndarray of shape (m, nx) containing the input data
+    labels: one-hot numpy.ndarray of shape (m, classes) containing
+        the labels of data
+    batch_size: size of the batch used for mini-batch gradient descent
+    epochs: number of passes through data for mini-batch gradient descent
+    verbose: boolean that determines if output should be printed
+        during training
+    shuffle: boolean that determines whether to shuffle the batches
+        every epoch
 
     Returns: the History object generated after training the model
     """
-    history = network.fit(x=data, y=labels,
-                          batch_size=batch_size,
-                          epochs=epochs,
-                          verbose=verbose,
-                          shuffle=shuffle)
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
     return history
