fb121b913b4d3d52437cfe11b0711d5a0bb9c2ee
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-08-15 11:02:49 +0400

    Update 5-train.py

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-"""
-Train a model using mini-batch gradient descent.
-"""
-
+"""Trains a model using mini-batch gradient descent, with validation data"""
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, verbose=True, shuffle=False):
+def train_model(
+        network, data, labels, batch_size, epochs,
+        validation_data=None, verbose=True, shuffle=False):
     """
-    Trains a neural network.
+    Trains a model using mini-batch gradient descent, and analyzes
+    validaiton data, if provided
 
-    Args:
-        network: the model to train
-        data: input data
-        labels: one-hot labels
-        batch_size: mini-batch size
-        epochs: number of epochs
-        validation_data: data to validate with
-        verbose: whether to print progress
-        shuffle: whether to shuffle data
+    network: the model to train
+    data: numpy.ndarray of shape (m, nx) containing the input data
+    labels: one-hot numpy.ndarray of shape (m, classes) containing
+        the labels of data
+    batch_size: size of the batch used for mini-batch gradient descent
+    epochs: number of passes through data for mini-batch gradient descent
+    validation_data: data to validate the model with, if not None
+    verbose: boolean that determines if output should be printed
+        during training
+    shuffle: boolean that determines whether to shuffle the batches
+        every epoch
 
-    Returns:
-        The History object generated after training.
+    Returns: the History object generated after training the model
     """
-    return network.fit(
-        x=data,
-        y=labels,
+    history = network.fit(
+        data,
+        labels,
         batch_size=batch_size,
         epochs=epochs,
         validation_data=validation_data,
         verbose=verbose,
         shuffle=shuffle
     )
+
+    return history
