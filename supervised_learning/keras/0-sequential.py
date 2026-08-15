e08b565e94ae828ea933012ab423c660045fe782
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-08-15 10:59:03 +0400

    Update 0-sequential.py

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,41 +1,40 @@
 #!/usr/bin/env python3
-"""
-Builds a neural network with the Keras library.
-"""
+"""Builds a neural network with the Keras library"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    Builds a neural network model.
-    Args:
-        nx: number of input features
-        layers: list containing the number of nodes in each layer
-        activations: list containing activation functions for each layer
-        lambtha: L2 regularization parameter
-        keep_prob: probability that a node will be kept
-    Returns:
-        A Keras Sequential model.
+    Builds a neural network with the Keras library
+
+    nx: number of input features to the network
+    layers: list containing the number of nodes in each layer
+    activations: list containing the activation functions for each layer
+    lambtha: L2 regularization parameter
+    keep_prob: probability that a node will be kept for dropout
+
+    Returns: the keras model
     """
-    model = K.Sequential()
     regularizer = K.regularizers.l2(lambtha)
+
+    model = K.Sequential()
+
     for i in range(len(layers)):
         if i == 0:
-            model.add(
-                K.layers.Dense(
-                    units=layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=regularizer,
-                    input_shape=(nx,)
-                )
-            )
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer,
+                input_shape=(nx,)
+            ))
         else:
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer
+            ))
+
+        if i != len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
-            model.add(
-                K.layers.Dense(
-                    units=layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=regularizer
-                )
-            )
+
     return model
