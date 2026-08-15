27e6c0942dadbd3cc642e75fdc581155d6a0cece
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-08-15 10:59:41 +0400

    Update 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
-"""
-Builds a neural network using the Keras Functional API.
-"""
+"""Builds a neural network with the Keras library using the Input class"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    Builds a neural network.
-    Args:
-        nx: number of input features
-        layers: list containing the number of nodes in each layer
-        activations: list containing the activation functions for each layer
-        lambtha: L2 regularization parameter
-        keep_prob: probability that a node will be kept
-    Returns:
-        A Keras model.
+    Builds a neural network with the Keras library
+
+    nx: number of input features to the network
+    layers: list containing the number of nodes in each layer
+    activations: list containing the activation functions for each layer
+    lambtha: L2 regularization parameter
+    keep_prob: probability that a node will be kept for dropout
+
+    Returns: the keras model
     """
+    regularizer = K.regularizers.l2(lambtha)
+
     inputs = K.Input(shape=(nx,))
     x = inputs
+
     for i in range(len(layers)):
         x = K.layers.Dense(
-            units=layers[i],
+            layers[i],
             activation=activations[i],
-            kernel_initializer=K.initializers.he_normal(seed=0),
-            kernel_regularizer=K.regularizers.l2(lambtha)
+            kernel_regularizer=regularizer
         )(x)
+
         if i != len(layers) - 1:
-            x = K.layers.Dropout(rate=1 - keep_prob)(x)
+            x = K.layers.Dropout(1 - keep_prob)(x)
+
     model = K.Model(inputs=inputs, outputs=x)
+
     return model
