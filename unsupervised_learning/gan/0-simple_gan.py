8a9758ae62eee7b47f9723bb03226947d80da801
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-08-31 17:13:01 +0400

    Update 0-simple_gan.py

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -12,23 +12,30 @@ class Simple_GAN(keras.Model):
     def __init__(self, generator, discriminator, latent_generator,
                  real_examples, batch_size=200, disc_iter=2,
                  learning_rate=.005):
-        super().__init__()                         # run the __init__ of keras.Model first.
+        # run the __init__ of keras.Model first.
+        super().__init__()
         self.latent_generator = latent_generator
-        self.real_examples    = real_examples
-        self.generator        = generator
-        self.discriminator    = discriminator
-        self.batch_size       = batch_size
-        self.disc_iter        = disc_iter
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
 
         self.learning_rate = learning_rate
-        self.beta_1 = .5                            # standard value, but can be changed if necessary
-        self.beta_2 = .9                            # standard value, but can be changed if necessary
+        # standard value, but can be changed if necessary
+        self.beta_1 = .5
+        # standard value, but can be changed if necessary
+        self.beta_2 = .9
 
         # define the generator loss and optimizer:
-        self.generator.loss      = lambda x: tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape))
+        self.generator.loss = lambda x: tf.keras.losses.MeanSquaredError()(
+            x, tf.ones(x.shape))
         self.generator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.generator.compile(optimizer=generator.optimizer, loss=generator.loss)
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2)
+        self.generator.compile(
+            optimizer=generator.optimizer, loss=generator.loss)
 
         # define the discriminator loss and optimizer:
         self.discriminator.loss = lambda x, y: (
@@ -36,14 +43,18 @@ class Simple_GAN(keras.Model):
             tf.keras.losses.MeanSquaredError()(y, -1 * tf.ones(y.shape))
         )
         self.discriminator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer, loss=discriminator.loss)
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2)
+        self.discriminator.compile(
+            optimizer=discriminator.optimizer, loss=discriminator.loss)
 
     # generator of fake samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
         if not size:
             size = self.batch_size
-        return self.generator(self.latent_generator(size), training=training)
+        return self.generator(
+            self.latent_generator(size), training=training)
 
     # generator of real samples of size batch_size
     def get_real_sample(self, size=None):
